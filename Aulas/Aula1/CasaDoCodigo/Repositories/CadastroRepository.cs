70df4d17b28ffc74a2928e700172e482524ac4eb
rodrigodesousa <rodrigudesousa@gmail.com>
2021-08-20 13:35:42 -0400

    Repository das outras tabelas, Session, Routes e Cache

---
+++ Aulas/Aula1/CasaDoCodigo/Repositories/CadastroRepository.cs
@@ -0,0 +1,19 @@
+﻿using CasaDoCodigo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Repositories
+{
+    interface ICadastroRepository
+    {
+
+    }
+    public class CadastroRepository : BaseRepository<Cadastro>, ICadastroRepository
+    {
+        public CadastroRepository(ApplicationContext contexto) : base(contexto)
+        {
+        }
+    }
+}
--- Aulas/Aula1/CasaDoCodigo/Startup.cs
+++ Aulas/Aula1/CasaDoCodigo/Startup.cs
@@ -25,6 +25,9 @@ namespace CasaDoCodigo
         {
             services.AddMvc();
 
+            services.AddDistributedMemoryCache();
+            services.AddSession();
+
             string connectionString = Configuration.GetConnectionString("Default");
             services.AddDbContext<ApplicationContext>(options =>
                 options.UseSqlServer(connectionString)
@@ -33,6 +36,9 @@ namespace CasaDoCodigo
             services.AddTransient<IDataService, DataService>();
 
             services.AddTransient<IProdutoRepository, ProdutoRepository>();
+            services.AddTransient<IPedidoRepository, PedidoRepository>();
+            services.AddTransient<ICadastroRepository, CadastroRepository>();
+            services.AddTransient<IItemPedidoRepository, ItemPedidoRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,11 +57,13 @@ namespace CasaDoCodigo
 
             app.UseStaticFiles();
 
+            app.UseSession();
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Pedido}/{action=Carrossel}/{id?}");
+                    template: "{controller=Pedido}/{action=Carrossel}/{codigo?}");
             });
 
             // serviceProvider.GetService<ApplicationContext>().Database.EnsureCreated(); -  Criar banco sem migration
